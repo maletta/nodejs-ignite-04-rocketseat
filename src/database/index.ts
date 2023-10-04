9f9001ba99c3ae9d90a563685692bcf60d3d9602
Maletta <mauricio_maletta@hotmail.com>
2023-10-04 15:44:02 -0300

    add network configuration to docker compose file

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,4 +2,11 @@ import { createConnection } from 'typeorm';
 
 // "createConnection" está deprecated, trocar essa função por DataSource
 
-createConnection();
+createConnection()
+  .then(() => {
+    console.log('CONECTADO NO BANCO DE DADOS COM SUCESSO');
+  })
+  .catch((err) => {
+    console.log('ERRO AO CONECTAR NO BANCO');
+    console.log(err);
+  });
