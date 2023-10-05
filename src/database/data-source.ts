4f0e472f433878da5af0eabc849d25c714b76e24
Maletta <mauricio_maletta@hotmail.com>
2023-10-05 14:21:20 -0300

    add migrations

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,26 +1,26 @@
-import { DataSource } from 'typeorm';
+// import { DataSource } from 'typeorm';
 
-// Novo modo de iniciar uma conexão com typeorm pois "createConnection" está deprecated
+// // Novo modo de iniciar uma conexão com typeorm pois "createConnection" está deprecated
 
-export const AppDataSource = new DataSource({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'docker',
-  password: 'password',
-  database: 'ignite-04',
-  synchronize: true, // atualiza a base de dados conforme as migrations ? pode ser perigoso
-  logging: true,
-  entities: [],
-  subscribers: [],
-  migrations: [],
-});
+// export const AppDataSource = new DataSource({
+//   type: 'postgres',
+//   host: 'localhost',
+//   port: 5432,
+//   username: 'docker',
+//   password: 'password',
+//   database: 'ignite-04',
+//   synchronize: true, // atualiza a base de dados conforme as migrations ? pode ser perigoso
+//   logging: true,
+//   entities: [],
+//   subscribers: [],
+//   migrations: [],
+// });
 
-AppDataSource.initialize()
-  .then(() => {
-    console.log('Banco de dados iniciado com sucesso');
-  })
-  .catch((error) => {
-    console.log('Erro ao iniciar o banco de dados');
-    console.log(error);
-  });
+// AppDataSource.initialize()
+//   .then(() => {
+//     console.log('Banco de dados iniciado com sucesso');
+//   })
+//   .catch((error) => {
+//     console.log('Erro ao iniciar o banco de dados');
+//     console.log(error);
+//   });
